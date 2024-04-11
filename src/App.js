d0e900bfff6db818b2a8bc846bb0634d00c1568d
D'Angelo <dangelokwakye@gmail.com>
2024-04-11 18:39:28 +0000

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Mail } from "./pages/mail";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Mail /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/pages/compose-message.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ComposeMessage = () => {
+  return (
+    <div>ComposeMessage</div>
+  )
+}
