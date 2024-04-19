24ef26f1403404b8acd2f45faa9cd46d0eb42877
D'Angelo <dangelokwakye@gmail.com>
2024-04-19 06:40:29 +0000

    update on dynamic messages display

--- src/components/inbox-lists.jsx
+++ src/components/inbox-lists.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const InboxLists = ({ sidebarToggle, setsetUserPage, userPage }) => {
+
+ const messages = [
+    {titie: "Meta Open Source", detail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, doloribus perferendis! Unde magni similique optio facilis esse debitis a magnam non, inventore assumenda voluptatem. Labore rem illo repellat, officia possimus porro alias quibusdam quos, quia maxime cupiditate libero dicta reiciendis cumque, eligendi mollitia accusamus velit. Velit vel cupiditate at totam?"},
+    {titie: "Google password reset", detail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, doloribus perferendis! Unde magni similique optio facilis esse debitis a magnam non, inventore assumenda voluptatem."},
+    {titie: "Big D", detail: "optio facilis esse debitis a magnam non, inventore assumenda voluptatem. Labore rem illo repellat, officia possimus porro alias quibusdam quos, quia maxime cupiditate libero dicta reiciendis cumque, eligendi mollitia accusamus velit. Velit vel cupiditate at totam?"},
+  ]
+
+
+
   return (
     <div className={`${sidebarToggle ? "" : "ml-[290px]"} w-89 text-left`}>
       <div className="h-[91vh] w-[260px] bg-white rounded-lg mx-1">
@@ -10,40 +19,21 @@ export const InboxLists = ({ sidebarToggle, setsetUserPage, userPage }) => {
           <hr className="bg-blsck h-1" />
         </div>
         <ul className="text-black">
-          <li className="px-2 flex items-center w-full space-between mb-2 hover:shadow hover:bg-violet-100 focus:bg-violet-100 py-2">
-            <div className="text-center pt-2 bg-green-500 m-2 rounded-full w-24 h-10">
-              A
+          {messages.map((message)=>{
+            return (
+              <li className="px-2 flex items-center w-full space-between mb-2 hover:shadow hover:bg-violet-100 focus:bg-violet-100 py-2">
+            <div className="text-center items-center p-2 bg-green-500 m-1 rounded-full w-[15%] h-10">
+              {message.titie[0]}
             </div>
-            <div className="px-2 truncate">
+            <div className="px-2 w-[85%] truncate">
               <Link className="font-bold">
-                <span>Meta Open Source</span>
+                <span>{message.titie}</span>
               </Link>
-              <p>mail preview hshgshbshbsjsbhshsbhbbhbchbchb gsgsvh</p>
+              <p>{message.detail}</p>
             </div>
           </li>
-
-          <li className="px-2 flex items-center w-full space-between mb-2 hover:shadow hover:bg-violet-100 focus:bg-violet-100 py-2">
-            <div className="text-center pt-2 bg-blue-500 m-2 rounded-full w-24 h-10">
-              G
-            </div>
-            <div className="px-2 truncate">
-              <Link className="font-bold">
-                <span>Google password reset</span>
-              </Link>
-              <p>mail preview hshgshbshbsjsbhshsbhbbhbchbchb gsgsvh</p>
-            </div>
-          </li>
-          <li className="px-2 flex items-center w-full space-between mb-2 hover:shadow hover:bg-violet-100 focus:bg-violet-100 py-2">
-            <div className="text-center pt-2 bg-violet-500 m-2 rounded-full w-24 h-10">
-              B
-            </div>
-            <div className="px-2 truncate">
-              <Link className="font-bold">
-                <span>Big D</span>
-              </Link>
-              <p>mail preview hshgshbshbsjsbhshsbhbbhbchbchb gsgsvh</p>
-            </div>
-          </li>
+            )
+          })}
         </ul>
       </div>
     </div>
